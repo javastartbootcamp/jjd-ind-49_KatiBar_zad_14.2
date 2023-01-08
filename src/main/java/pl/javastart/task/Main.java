166bd2f9ddc8a642105e299ead6a6d16c4b72092
KatiBar <katarzyna.kinga.baranska@gmail.com>
2023-01-08 20:27:00 +0100

    Dodanie klasy Car oraz ServiceUtility, w której znajduje się logika programu.

--- src/main/java/pl/javastart/task/Main.java
+++ src/main/java/pl/javastart/task/Main.java
@@ -1,8 +1,16 @@
 package pl.javastart.task;
 
+import java.io.IOException;
+
 public class Main {
 
     public static void main(String[] args) {
-
+        String fileName = "CarsQueue.csv";
+        ServiceUtility serviceUtility = new ServiceUtility();
+        try {
+            serviceUtility.run(fileName);
+        } catch (IOException e) {
+            System.out.println("Sprawdź, czy podałeś poprawną nazwę pliku: " + fileName);
+        }
     }
 }
